42ac1f1f8216cb7d0574b14249d1b4fd15dbc737
pkotengpoku <135516423+pkotengpoku@users.noreply.github.com>
2023-11-19 18:50:35 +0000

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors({
-  origin: ['http://localhost:5173','http://localhost:4173','https://volary.netlify.app','https://voshop.netlify.app/'],
+  origin: ['http://localhost:5173','http://localhost:4173','https://volary.netlify.app','https://voshop.netlify.app'],
   credentials: true
 }));
 
